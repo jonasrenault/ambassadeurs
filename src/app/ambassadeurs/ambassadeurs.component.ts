e4f02cf1635e0a52be5e73ef75d563a53ba66b2a
Jonas Renault <jonas.renault@inria.fr>
2016-09-26 16:31:40 +0200

    Filtering the list

--- src/app/ambassadeurs/ambassadeurs.component.ts
+++ src/app/ambassadeurs/ambassadeurs.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AmbassadeursService } from '../ambassadeurs.service';
 import { Ambassadeur } from '../ambassadeur';
+import { FormControl } from '@angular/forms';
+
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-ambassadeurs',
@@ -10,11 +13,28 @@ import { Ambassadeur } from '../ambassadeur';
 export class AmbassadeursComponent implements OnInit {
 
   ambassadeurs: Ambassadeur[];
+  filteredAmbassadeurs: Ambassadeur[];
+  filter: Ambassadeur = {name: '', country: '', year:null};
+  nameControl = new FormControl();
+  countryControl = new FormControl();
+  yearControl = new FormControl();
 
   constructor(private ambassadeursService: AmbassadeursService) { }
 
   ngOnInit() {
     this.getAmbassadeurs();
+    this.countryControl.valueChanges.debounceTime(300).subscribe(newValue => {
+      this.filter.country = newValue;
+      this.doFilter();
+    });
+    this.yearControl.valueChanges.debounceTime(300).subscribe(newValue => {
+      this.filter.year = newValue;
+      this.doFilter();
+    });
+    this.nameControl.valueChanges.debounceTime(300).subscribe(newValue => {
+      this.filter.name = newValue;
+      this.doFilter()
+    });
   }
 
   getAmbassadeurs(): void {
@@ -22,7 +42,17 @@ export class AmbassadeursComponent implements OnInit {
         .getAmbassadeurs()
         .then(ambassadeurs => {
           this.ambassadeurs = ambassadeurs;
+          this.doFilter();
         });
+  }
+
+  doFilter(): void {
+    console.log('filtering');
+    const nameFilter = this.filter.name ? this.filter.name.toLowerCase() : null;
+    const countryFilter = this.filter.country ? this.filter.country.toLowerCase() : null;
+    this.filteredAmbassadeurs = this.ambassadeurs.filter(element => {
+      return (!nameFilter || element.name.toLowerCase().indexOf(nameFilter) !== -1) && (!countryFilter || element.country.toLowerCase().indexOf(countryFilter) !== -1) && (!this.filter.year || element.year === this.filter.year);
+    });
   }
 
 }
